21f602ce31af38da3518dc8b0b957e5c84560dd3
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-13 19:17:44 +0100

    Create 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - prints times table
+ * Return:void
+ */
+
+void times_table(void)
+{	
+	char wat = '0, '
+	for(int z = 1; z <= 8; z++)
+		_putchar('wat');
+	_putchar('0');
+	_putchar('\n');
+	for(int b = 1; b <= 9; b++)
+	{
+		for(int a = 1; b <= 8; b++)
+		{
+			_putchar('wat');
+ 			int c = a * b;
+
+			_putchar(c);
+			_putchar(',');
+			_putchar(' ');
+			a++
+		}		
+		_putchar('c');
+		_putchar(\n);
+		b++
+	}		
+ }
+		
+			
