99ccfc27bbcf41b74a4b24928550b3e40f530b0a
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-13 23:45:55 +0100

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: computes and prints the sum of all the multiples of 3 or
+ * Description: computes and prints even  number < 4,000,000
  * 5 below 1024 (excluded), followed by a new line
  * Return: 0
  */
 int main(void)
 {
-	int a = 0, b = 1, next, sum = 0;
+	int a = 0, b = 1, next = 0;
+	int sum = 0;
 
 	while (next < 4000000)
 	{
 		next = a + b;
 		a = b;
 		b = next;
-		
 		if (next % 2 == 0)
-    			sum += next;
-
+			sum += next;
 	}
-	printf("%i", sum);
-	putchar('\n');
+	printf("%i\n", sum);
 	return (0);
 }
