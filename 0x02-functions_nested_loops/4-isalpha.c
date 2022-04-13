2d6580c8606159ccba00f3f773339bd3ff21bc6f
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-13 13:47:51 +0100

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isalpha - entry point
+* @c : character to be checked
+* Return: 0 or 1
+int _isalpha(int c)
+{
+    if(c >= 'a' && c <= 'z')
+        return (1);
+    else if(c >= 'A' && c <= 'A')
+        return (1);
+    else
+        return (0);
+
+}
