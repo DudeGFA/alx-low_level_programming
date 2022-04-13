8904dbea788cc941749b71f822951c71fd715b9d
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-13 15:30:03 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
-* int print_sign - prints the sign on a number
+* print_sign - prints the sign on a number
 * @n : character to be checked
 * Return: 0 or 1
 */
 int print_sign(int n)
 {
 	int p;
-	if (n > '0')
+	
+	if (n > 0)
 	{
- 		_putchar('+');
+		_putchar('+');
         	p = 1;
 	}
-	else if (n < '0')
+	else if (n < 0)
 	{
 		_putchar('-');
 		p = -1;
