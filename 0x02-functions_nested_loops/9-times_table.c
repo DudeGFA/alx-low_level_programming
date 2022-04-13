8ec33f43e4f0930c6240b199db16053e7cdc4a4c
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-13 19:22:05 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,16 +7,24 @@
 
 void times_table(void)
 {	
-	char wat = '0, '
-	for(int z = 1; z <= 8; z++)
-		_putchar('wat');
 	_putchar('0');
+	_putchar(',');
+	_putchar(' ');
+	for(int z = 1; z <= 8; z++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		_putchar('0');
+	}
 	_putchar('\n');
 	for(int b = 1; b <= 9; b++)
 	{
 		for(int a = 1; b <= 8; b++)
 		{
-			_putchar('wat');
+			_putchar('0');
+			_putchar(',');
+			_putchar(' ');
  			int c = a * b;
 
 			_putchar(c);
