63086409f544f8ea0be345ffc96297d98815529d
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-13 20:42:36 +0100

    Create 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+void print_to_98(int n);
+if (n > 98 && n != 98)
+{
+	while (n > 98 && n != 98)
+		printf("%d, ", n);
+		n--;
+	_putchar(n);
+}
+if (n < 98 && n !+ 98)
+{
+	while (n < 98 && n != 98)
+		printf("%d, ", n);
+		n++;
+	_putchar(n);
+}
+
+if (n=98)
+	_putchar(n);
