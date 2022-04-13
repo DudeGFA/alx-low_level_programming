26ac1facb15367c57b5584f4feb33385d72d5deb
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-13 15:18:49 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,14 +7,21 @@
 */
 int print_sign(int n)
 {
+	int p;
 	if (n >= '0')
+	{
  		_putchar('+');
-        	return (1);
+        	p = '1';
+	}
 	else if (n <= '0')
-		putchar('-');
-		return (-1);
+	{
+		_putchar('-');
+		p = '-1';
+	}
 	else
-		putchar('0');
-		return (0);
-
+	{
+		_putchar('0');
+		p = '0';
+	}
+	return (p)
 }
