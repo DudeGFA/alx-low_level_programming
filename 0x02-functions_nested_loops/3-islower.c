4022ca64123930199a50fa3478c66ffb2944dc9e
DudeGFA <favouridowuagida0@gmail.com>
2022-04-13 05:13:14 -0700

    task2b

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _islower - entry point
+* identifies lower case letters
+* return: 0 or 1
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1)
+	else
+		return (0);
+}
+
