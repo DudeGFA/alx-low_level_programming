cf54d1366850b1e694401ae1c45f50c167ced11c
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-13 13:56:18 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,9 +7,9 @@
 */
 int _isalpha(int c)
 {
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
-	else if(c >= 'A' && c <= 'Z')
+	else if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
