2137864d16a8f47ab67fd1d0a8b2f74009971354
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-13 23:32:18 +0100

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,15 +7,14 @@
  */
 int main(void)
 {
-	int i = 0;
 	long int a = 0, b = 1, next, sum = 2;
 
-	while (next < 4,000,000)
+	while (next < 4000000)
 	{
 		next = a + b;
 		a = b;
 		b = next;
-		if ( next % 2 = 0)
+		if ((next % 2) = 0)
     			sum = sum + next;
 		
 	}
