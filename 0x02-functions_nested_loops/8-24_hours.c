d8dfd4ef41abbd8eabfbd31e4c5f15d351dc94b3
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-13 17:40:40 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,26 +6,30 @@
  */
 void jack_bauer(void)
 {
-	int a = 0
+	int a = 0;
+	
 	while (a < 3)
 	{
 		if (a < 2)
 		{
 			int b = 0
+				
 			while (b < 10)
 			{
 				int c = 0
+					
 				while (c < 6)
 				{
 					int d = 0
+
 					while (d < 10)
 					{
-					putchar(a)
-					_putchar(b)
-					_putchar(':')
-					_putchar(c)
-					_putchar(d)
-					_putchar(\n)
+					 _putchar('0' + a)
+					 _putchar('0' + b)
+					 _putchar(':')
+					 _putchar('0' + c)
+					 _putchar('0' + d)
+					 _putchar('\n')
 					d++
 					}
 					c++
@@ -37,20 +41,23 @@ void jack_bauer(void)
 		else
 		{
 			int b = 0
+
 			while (b < 4)
 			{
 				int c = 0
+
 				while (c < 6)
 				{
 					int d = 0
+
 					while (d < 10)
 					{
-					putchar(a)
-					_putchar(b)
-					_putchar(':')
-					_putchar(c)
-					_putchar(d)
-					_putchar(\n)
+					 _putchar('0' + a)
+					 _putchar('0' + b)
+					 _putchar(':')
+					 _putchar('0' + c)
+					 _putchar('0' + d)
+					 _putchar('\n')
 					d++
 					}
 					c++
@@ -59,4 +66,6 @@ void jack_bauer(void)
 			}
 			a++
 		}
+	}
+}
 	
