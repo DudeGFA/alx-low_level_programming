00d5b2615589ec38a9c781be818ec18bc00ab55b
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-13 21:05:45 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,20 +7,22 @@
  * Return:0 or 1
  */
 void print_to_98(int n);
-if (n > 98 && n != 98)
 {
+	if (n > 98 && n != 98)
+	{
 	while (n > 98 && n != 98)
 		printf("%d, ", n);
 		n--;
 	_putchar(n);
-}
+	}
 else if (n < 98 && n != 98)
-{
+	{
 	while (n < 98 && n != 98)
 		printf("%d, ", n);
 		n++;
 	_putchar(n);
-}
+	}
 
 else (n=98)
 	_putchar(n);
+}
