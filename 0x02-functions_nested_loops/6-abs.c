ab0c8143231fcc80550d6a930033fe86c6bbb389
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-13 15:43:29 +0100

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_sign - prints the sign on a number
+* @n : character to be checked
+* Return: 0 or 1
+*/
+int _abs(int)
+{
+	int n;
+
+	if (n > 0)
+	{
+		_putchar(n);
+	}
+	else if (n < 0)
+	{
+		n = n*(-1)
+		_putchar(n);
+	}
+	else
+	{
+		_putchar('0');
+	}
+	return (n);
+}
