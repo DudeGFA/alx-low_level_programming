0bb0bd639ee2cf32e37fe78c3d433ff3222220d1
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-14 20:39:34 +0100

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_line - prints _
+ * @n : paramater depicting no of '_'
+ */
+void print_line(int n)
+	int i;
+
+	for (i = 1; i <= n; i++)
+		_printchar('_');
+	_printchar('\n');
