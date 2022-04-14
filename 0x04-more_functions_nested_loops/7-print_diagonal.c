4f702008fd3135aca410d795c9a7606e971f8dbc
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-14 21:07:06 +0100

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * print_diagonal - prints \
+ * print_diagonal - prints '\'
  * @n : paramater depicting no of '\'
  */
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	for (i = 1; i <= n; i++)
 	{
-		_putchar('\');
+		_putchar('\\');
 		_putchar('\n');
+		for(j = 0; j = i;)
+			_putchar(' ');
 	}
 }
