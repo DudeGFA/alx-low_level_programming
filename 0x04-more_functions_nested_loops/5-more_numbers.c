931e3c0ad222183ec86de0ea1fde5010bcd58b76
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-14 20:15:16 +0100

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *more_numbers - prints numbers
+ */
+void more_numbers(void)
+	int i;
+	int b;
+	for (i = 1; i <= 10; i++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			_putchar('0' + b)
+		}
+		_putchar('\n')
+	}
