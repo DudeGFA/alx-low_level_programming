387750921f1e40db0f171780746a2d6593549a52
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-21 00:44:50 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,4 +1,4 @@
-​#​include​ ​"​main.h​" 
+ ​#​include​ ​"​main.h​" 
  ​/*​* 
  ​ * swap_int - swap variable values 
  ​ * @a: pointer 1 
@@ -11,25 +11,4 @@
  ​        *a += *b; 
  ​        *b = *a - *b; 
  ​        *a = *a - *b; 
- ​}
-
-task 2: 2-strlen.c
-
- ​#​include​ ​"​main.h​" 
-  
- ​/*​* 
- ​ * _strlen - find the length of a string 
- ​ * @s: pointer to the string to check 
- ​ * Return: void 
- ​*/ 
-  
-  
- ​int​ ​_strlen​(​char​ *s) 
- ​{ 
- ​        ​int​ i = ​0​; 
-  
- ​        ​while​ (s[i]) 
- ​                i++; 
-  
- ​        ​return​ (i); 
  ​}
