5a41bef6886b71ccfc249e13badce298f784e572
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-21 01:24:52 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,10 @@
- ​#​include​ ​"​main.h​" 
- ​/*​* 
- ​ * reset_to_98 - reset argument to 98 
- ​ * @n: pointer 
- ​ * Return: void 
- ​*/ 
-  
- ​void​ ​reset_to_98​(​int​ *n) 
- ​{ 
-  
- ​        *n = ​98​; 
-  
- ​}
+#include "main.h"
+/**
+* reset_to_98 - updates the value it points
+*@n: pointer of the direcction at the variable n
+*Return: 0 (success)
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
