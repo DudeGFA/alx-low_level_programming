24fb9c17f4b01a732da1ee499d2e25e1284d31d1
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-25 03:17:50 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,8 +9,12 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	for (i = 0; s[i]; i++)
-		if ((s[i] >= a) && ([i] <= z))
-			s[i] -= 32;
+	for (i = 0; *(s + i); i++)
+	{
+		if ((*(s + i) >= a) && (*(s + i) <= z))
+		{
+			*(s + i) -= 32;
+		)
+	}
 return (s);
 }
