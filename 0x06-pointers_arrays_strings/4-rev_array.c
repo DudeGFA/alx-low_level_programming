040d2ddc344f5d82f22beef6af833eb6b09c7c06
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-25 01:54:11 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,6 +3,7 @@
  * reverse_array - reverses a string
  * @a - pointer to an array
  * @n - length of array
+ * Return: nothing
  */
  
  void reverse_array(int *a, int n)
@@ -11,5 +12,5 @@
  
 	 while (i <= n/2);
 		c = a[i];
-		a[i] a[n - 1 - i];
+		a[i] = a[n - 1 - i];
 		a[n - 1 - 1] = c;
