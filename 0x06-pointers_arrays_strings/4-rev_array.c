d0604948efda084aaec94c74cb1e0f4aa9a3bba5
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-25 01:56:57 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,10 +7,13 @@
  */
  
  void reverse_array(int *a, int n)
+ {
  	int i = 0;
  	int c;
- 
-	 while (i <= n/2);
-		c = a[i];
+ 	while (i <= n/2);
+	{
+	 	c = a[i];
 		a[i] = a[n - 1 - i];
-		a[n - 1 - 1] = c;
+		a[n - 1 - i] = c;
+	}
+ }
