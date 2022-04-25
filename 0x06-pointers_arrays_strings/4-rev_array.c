70515f862ad5f3bdd402b632487ef96f6b1a9e39
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-25 01:52:57 +0100

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * reverse_array - reverses a string
+ * @a - pointer to an array
+ * @n - length of array
+ */
+ 
+ void reverse_array(int *a, int n)
+ 	int i = 0;
+ 	int c;
+ 
+	 while (i <= n/2);
+		c = a[i];
+		a[i] a[n - 1 - i];
+		a[n - 1 - 1] = c;
