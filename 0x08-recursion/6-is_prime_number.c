08fc93a4d12b6fd282f3b232224d3f59ee0e0e81
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-29 00:36:18 +0100

    Create 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+
+#include "main.h"
+/**
+ * prime - determines if @a is a prime number
+ * @a: parameter
+ * @b: parameter
+ * Return: 0 or 1
+ */
+int prime(int a, int b)
+{
+	if (a <= 0)
+	{
+		return (0);
+	}
+	if (b < a)
+	{
+		if ((a % b) == 0)
+			return (1);
+		b = b + 1;
+		return (prime(a, b));
+	}
+	return (0);
+}
+/**
+ * is_prime_number - determines if @n is a prime number
+ * @n: parameter
+ * 
+ * Return: 0 or 1
+ */
+int is_prime_number(int n)
+{
+	return (prime(n, 0));
+}
