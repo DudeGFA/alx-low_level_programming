732a4aab74816fbac2685cce550dae1ff1776738
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-28 23:16:54 +0100

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
- * _pow_recursion - returns x**y
+ * _pow_recursion - returns x^y
  * @x: parameter
  * @y: parameter
- * Return: x**y
+ * Return: x^y
  */
 int _pow_recursion(int x, int y)
 {
-	int p = x**y;
-
+	int d = y - 1;
+	
+	if (d == 1)
+		return (x);
+	int p = x * _pow_recursion(int x, int d);
 	return(p);
 }
