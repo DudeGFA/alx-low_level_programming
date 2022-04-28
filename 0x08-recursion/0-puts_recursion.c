ca7d3931cd9fead650d8da2842c8f862a4ee38f4
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-04-28 21:41:41 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,16 @@
 #include "main.h"
 /**
  * _puts_recursion - prints a string
+ * @s: string
  * Return: void
  */
 void _puts_recursion(char *s)
 {
-    printf("%s\n", *s);
+	int i;
+	
+	while(*(s + i) != '\0')
+	{
+		_putchar(*(s + i))
+		i++
+	}
 }
