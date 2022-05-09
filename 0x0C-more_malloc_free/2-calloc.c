e32cf63e04e28b451726cd631ea55f6930f90345
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-09 19:09:57 +0100

    Create 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory and initialises it with 0
+ * @nmemb: parameter
+ * @size: parameter
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+	unsigned int p, i;
+
+	p = nmemb * size;
+	arr = malloc(p);
+	for(i = 0; i < p; i++)
+		arr[i] = 0
+	return (p);
+}
