e458b63cfb9834fa4d08233ad0f68f098416db67
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-09 16:53:34 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,18 +16,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; s1[i] != '\0'; i++)
-	for (j = 0; s2[j] != '\0' && j < n; j++)
-	str = malloc(i + j + 1);
+	if (s1 == "")
+		i = 0;
+	else
+		for (i = 0; s1[i] != '\0'; i++)
+	if (s2 == "")
+		j = 0;
+	else
+		for (j = 0; s2[j] != '\0'; j++)
+	str = malloc((i + j + 1) * sizeof(char));
+	if (n > j)
+		n = j;
 	if (str == NULL)
 	{
-		free(str);
-		return (NULL);
+		return (0);
 	}
 	for (k = 0; k < i; k++)
 		str[k] = s1[k];
-	for (l = k, a = 0; s2[a] != '\0' && a < n; a++, l++)
+	for (l = k, a = 0; a < n; a++, l++)
 		str[l] = s2[a];
-	str[l] = '\0';
+	str[i + j] = '\0';
 	return (str);
 }
