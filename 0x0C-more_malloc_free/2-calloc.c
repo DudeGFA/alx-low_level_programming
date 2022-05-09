34fb5aa5bc166cb899f84d37f57830086710b55c
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-09 21:03:22 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,11 +9,19 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr;
-	unsigned int p, i;
+	unsigned int p, i, n;
 
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
 	p = nmemb * size;
 	arr = malloc(p);
+	if (arr = NULL)
+	{
+		free(arr);
+		return (NULL);
+	}
+	n = 0;
 	for(i = 0; i < p; i++)
-		arr[i] = 0;
+		arr[i] = n;
 	return (arr);
 }
