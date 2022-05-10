23ea9b58de1f2a03dd69ce55e349f0f58f785951
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-10 01:10:42 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -7,7 +7,7 @@
  * @argv: array of cl arguments
  * Return: 0
  */
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i, j;
 	unsigned long num;
@@ -17,7 +17,7 @@ int main (int argc, char* argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	for ( i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
