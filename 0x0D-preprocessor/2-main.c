2659bdadbe35b9fac3160224617b616bdacdca66
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-11 19:34:12 +0100

    Create 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+/**
+ * main - prints file name
+ * @argc: no of cl arguments
+ * @argv: array of cl arguments
+ * Return: void
+ */
+void main(int argc, **char argv[])
+{
+	printf ("%s\n", argv[0]);
+}
