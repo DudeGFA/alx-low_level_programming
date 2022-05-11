1479b22e9c2d703150a64fc898d7873cfc4fe5de
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-12 00:49:28 +0100

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * dog - name,age and owner of a dog as attributes
+ * @name: name of the dog
+ * @age: dog's age
+ * @owner: name of dog's owner
+ *
+ * Description: describes a dog
+ */
+struct dog
+{
+	char *name
+	float age
+	char *owner
+};
