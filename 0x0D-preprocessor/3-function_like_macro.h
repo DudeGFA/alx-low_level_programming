97d32bde5dcf4a640b71303577ad22acdfdd8062
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-11 20:20:38 +0100

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,4 @@
-/**
- * ABS - returns the absolute value of a number
- * @x: argument
- * Return: absolute value of x
- */
-int ABS(int x)
-{
-	if (x < 0)
-		x = x * (-1);
-	return (x);
-}
+#ifndef _ABS_
+#define _ABS_
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+#endif
