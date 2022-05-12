7d2621a998577ca9fb1caf0e91dcda3604d41c40
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-12 02:35:03 +0100

    Create 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * dog_t *new_dog - function that creates a new dog
+ * @name: new dog's name
+ * @age: new dog's age
+ * @owner: new dog's owner
+ * Return: pointer to new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+
+	ptr = malloc(sizeof(dog_t));
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr->name = name;
+	ptr->age = age;
+	ptr->owner = owner;
+	return (ptr);
+}
