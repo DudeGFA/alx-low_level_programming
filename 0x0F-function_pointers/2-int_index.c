f74a54d69a5c429e6dbf7c86a62ae7758ca681bf
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-13 20:15:55 +0100

    Create 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include function_pointer.h
+/**
+ * int_index - returns index of a matching int
+ * @array: parameter
+ * @size: size of array
+ * @cmp: pointer to a function
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for(i = 0; i < size; i++)
+	{
+		if(cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
