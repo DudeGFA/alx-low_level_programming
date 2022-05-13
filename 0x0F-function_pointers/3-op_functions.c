e13a2e781ad5ce1050bfa59b26a05652d3b99543
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-13 21:50:35 +0100

    Create 3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+/**
+ * op_add - adds two ints
+ * @a: parameter
+ * @b: paramter
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substracts two ints
+ * @a: parameter
+ * @b: paramter
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies two ints
+ * @a: parameter
+ * @b: paramter
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two ints
+ * @a: parameter
+ * @b: paramter
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - returns remainder of div of 2 ints
+ * @a: parameter
+ * @b: paramter
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
