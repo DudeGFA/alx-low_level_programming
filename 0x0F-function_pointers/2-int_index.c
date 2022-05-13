790be207303fe1b7ea76a81a99ec3e2ba6e0adeb
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-13 20:21:15 +0100

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,6 +4,7 @@
  * @array: parameter
  * @size: size of array
  * @cmp: pointer to a function
+ * Return: index of matching int
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
@@ -13,9 +14,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		if (size <= 0)
 			return (-1);
-		for(i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
-			if(cmp(array[i]) != 0)
+			if (cmp(array[i]) != 0)
 				return (i);
 		}
 	}
