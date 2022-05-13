0c431619accbe3d8453a17b37f337a16b428d3c8
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-13 18:20:35 +0100

    Create 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_name: prints a name as it is or in uppercaser
+ * @name: paramter
+ * @f: pointer to a function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
