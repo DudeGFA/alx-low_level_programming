6dcf0234b9170e0d2fc641d9bd7d898a8035b2dd
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-13 18:01:36 +0100

    Update main.h

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef _main_h
+#define _main_h
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
