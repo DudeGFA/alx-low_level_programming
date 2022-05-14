c9b851bb22d29f42e0d2fbad01f75601967c856c
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-14 03:31:22 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,14 +10,10 @@ void main(int argc, char *argv[])
 
 	if (argc != 4)
 	{
-		printf(Error\n);
+		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2] != '+' && argv[2] != '-' && argv[2] != '%' && argv[2] != '/' && argv[2] != '*')
-	{
-		printf(Error\n);
-		exit(99);
-	}
-	p = get_op_func(char *s)(atoi(argv[1]), atoi(argv[3]))
+	p = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]))
 	printf("%d\n", p);
+	return (0);
 }
