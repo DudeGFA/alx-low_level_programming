5cf847adae739f08b4d8bd1790cb046fa4b71e82
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-14 03:21:54 +0100

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -13,16 +13,15 @@ int (*get_op_func(char *s))(int , int )
 	{"%", op_mod},
         {NULL, NULL}
 	};
-	int i;
-	op_t *ptr;
-	
-	ptr = ops;
-	i = 0;
-	while (i < 5)
+	int i = 0;
+
+	while (i < 6)
 	{
 		if (ops[i].op == argv[2])
-			return (ptr[i].f(argv[1], argv[3]));
+			return (ops[i].f);
+		i++
 	}
-	return (NULL);
+	printf("Error\n");
+	exit(99);
 }
 	
