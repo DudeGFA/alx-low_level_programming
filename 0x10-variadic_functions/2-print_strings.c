5132c57e3d0db8dd4c93e7ed60c3a52efce0cbdc
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-16 21:44:04 +0100

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%s", va_arg(ap, char[]));
-		if (i < (n - 1) && (separator != NULL))
+		if ((i < (n - 1)) && (separator != NULL))
 			printf("%s", separator);
 	}
 	va_end(ap);
