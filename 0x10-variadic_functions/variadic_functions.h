519ad5b85ad84e7628d3dc6bde19e085a385d53a
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-16 17:05:51 +0100

    Update and rename main.h to variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,8 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef _VARIADIC_FUNCTIONS_H
+#define _VARIADIC_FUNCTIONS_H
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-#endif /* main.h */
+#endif /* variadic_functions.h */
