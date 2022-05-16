1746a8c80ca89db6faab42248747c479d201d804
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-16 18:44:40 +0100

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,10 +14,9 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n <= 0)
 		return (0);
-		va_start(ap, n);
-		for (i = 0; i < n; i++)
-			 p += va_arg(ap, int);
-		va_end(ap);
-		return (p);
-	return (0);
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+		p += va_arg(ap, int);
+	va_end(ap);
+	return (p);
 }
