d62ca959f1e23e6e3db482e66ba01932be7cfcbb
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-17 02:52:06 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,15 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
+/**
+ * struct prints_all - prints_all
+ * @sign: represents data type
+ * @print: a pointer function
+ */
+typedef struct prints_all
+{
+	char *sign;
+	void (*print)(va_list ap);
+} printer;
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
