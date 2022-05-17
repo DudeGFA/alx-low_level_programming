7b01e52739c354298b84dfd3b775004cc2f10f60
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-17 02:45:08 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,38 +1,61 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+/**
+ * struct prints_all - prints_all
+ * @sign: represents data type
+ * @print: a pointer function
+ */
 typedef struct prints_all
 {
 	char *sign;
 	void (*print)(va_list ap);
 } printer;
+/**
+ * print_char - prints a char
+ * @ap: list of arguments of variadic function
+ */
 void print_char(va_list ap)
 {
 	char c;
 	c = va_arg(ap, int);
 	printf("%c", c);
 }
+/**
+ * print_int - prints a char
+ * @ap: list of arguments of variadic function
+ */
 void print_int(va_list ap)
 {
 	int d;
 	d = va_arg(ap, int);
 	printf("%d", d);
 }
+/**
+ * print_float - prints a float
+ * @ap: list of arguments of variadic function
+ */
 void print_float(va_list ap)
 {
 	float e;
 	e = va_arg(ap, double);
 	printf("%f", e);
 }
+/**
+ * print_string - prints a string
+ * @ap: list of arguments of variadic function
+ */
 void print_string(va_list ap)
 {
 	char *f;
 	f = va_arg(ap, char*);
+	if (string == NULL)
+		printf("(nil)");
 	printf("%s", f);
 }
 /**
  * print_all - print all parameters
- * @format: no of parameters
+ * @format: list of types of arguments passed to the function
  * Return: void
  */
 void print_all(const char * const format, ...)
