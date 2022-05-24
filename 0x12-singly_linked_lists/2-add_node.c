9ece47eba28ca671974c9977447aa5312f514406
DudeGFA <favouridowuagida0@gmail.com>
2022-05-24 23:23:27 +0100

    created 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+list_t *add_node(list_t **head, const char *str)
+{
+    char* t = strdup(str);
+    if (t == NULL)
+        free (t);
+        return (NULL);
+    /*(*head)->next = malloc(sizeof(list_t));*/
+    /*if ((*head)->next = NULL)
+        free ((*head)->next);
+        return (NULL);*/
+    (*head)->next->str = t;
+    return (*head);
+}
