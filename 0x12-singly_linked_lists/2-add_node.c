20f8ba01c4d78e19b35e19f6096e41d1c54a86f8
DudeGFA <favouridowuagida0@gmail.com>
2022-05-24 23:27:27 +0100

    ...

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,8 +3,10 @@ list_t *add_node(list_t **head, const char *str)
 {
     char* t = strdup(str);
     if (t == NULL)
+    {
         free (t);
         return (NULL);
+    }
     /*(*head)->next = malloc(sizeof(list_t));*/
     /*if ((*head)->next = NULL)
         free ((*head)->next);
