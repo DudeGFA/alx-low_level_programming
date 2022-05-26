9244f8c7933697f5cb1552d32e0b788e3fa2280b
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-26 19:47:44 +0100

    Create 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: head of singly linked list.
+ * Return: void
+ */
+
+void free_listint2(listint_t **head);
+{
+	listint_t *ptr;
+
+	while ((ptr = head) != NULL)
+	{
+		head = head->next;
+		free(ptr);
+	}
+}
