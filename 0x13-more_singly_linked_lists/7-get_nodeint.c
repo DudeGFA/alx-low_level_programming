1bdf620d0ccbece12ecb2e80b3a69f35da65eb96
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-26 21:26:43 +0100

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,7 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		if (ptr != NULL)
 		{
-			if (i = index)
+			if (i == index)
 				return (ptr);
 			ptr = ptr->next;
 		}
