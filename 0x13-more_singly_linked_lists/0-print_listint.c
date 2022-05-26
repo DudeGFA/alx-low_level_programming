a29fbf7c1c4c9d4d5b2bc6120b8115b0cea27c0e
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-26 16:01:30 +0100

    Create 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *print_listint - prints the members of a list
+ *
+ *@h: pointer to a linked list
+ *Return: size_t
+ */
+size_t print_listint(const listint_t *h);
+{
+	int i = 1;
+
+	if (h)
+	{
+		if (h != NULL)
+		{
+			if (h->int != NULL)
+				printf("%d\n", h->int);
+			else
+				printf("(nil)\n");
+			return (i + print_listint(h->next));
+		}
+	}
+	return (0);
+}
