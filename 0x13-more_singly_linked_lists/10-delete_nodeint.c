fb81639c5b0dd253e31deb38d7aa9d4587d66044
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-27 00:53:02 +0100

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		ptr = *head;
 		if (index != 0)
 		{
-			for (i = 0; i < idx && ptr != NULL; i++)
+			for (i = 0; i < index && ptr != NULL; i++)
 			{
 				node = ptr;
 				ptr = ptr->next;
@@ -33,5 +33,5 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		node->next = ptr->next;
 		return (1);
 	}
-	return (-1):
+	return (-1);
 }
