cf61ef7465af1e45e999e6d44d05b658c3696acf
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-26 23:07:01 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,7 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *ptr, *node, *temp;
+	listint_t *ptr, *node;
 	unsigned int i;
 
 	if (head && idx && n)
