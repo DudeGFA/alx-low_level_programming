b3c3c377dba65bc60c326b3d9b861737f28fbac4
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-26 21:18:40 +0100

    Create 7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - prints the value of node at an index
+ * @head: head of singly linked list.
+ * Return: void
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *ptr;
+	int i;
+
+	ptr = head;
+	for (i = 0; i <= index ; i++)
+	{
+		if (ptr != NULL)
+		{
+			if (i = index)
+				return (ptr->n);
+			ptr = ptr->next;
+		}
+	}
+	return (NULL);
+}
