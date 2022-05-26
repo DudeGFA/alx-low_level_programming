35dd7f848b008c57e15ee99772de5b391d2b292a
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-27 00:48:38 +0100

    Create 10-delete_nodeint.c

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts node at an index
+ * @head: head of singly linked list.
+ * @idx: node index
+ * @n: int
+ * Return: void
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+{
+	listint_t *ptr, *node;
+	unsigned int i;
+	
+	if (head)
+	{
+		ptr = *head;
+		if (index != 0)
+		{
+			for (i = 0; i < idx && ptr != NULL; i++)
+			{
+				node = ptr;
+				ptr = ptr->next;
+			}
+			if (ptr == NULL)
+				return (-1);
+		}
+		else
+		{
+			*head = (*head)->next;
+		}
+		node->next = ptr->next;
+		return (1);
+	}
+	return (-1):
+}
