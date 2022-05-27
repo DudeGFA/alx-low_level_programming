03c0e27a0c63995d6004ec2f50d60fa8f4b77a96
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-27 23:26:07 +0100

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -8,21 +8,35 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *tmp;
-	
-	if (head)
+	listint_t *p2;
+	listint_t *prev;
+
+	p2 = head;
+	prev = head;
+	while (head && p2 && p2->next)
 	{
-		while (head != NULL)
+		head = head->next;
+		p2 = p2->next->next;
+
+		if (head == p2)
 		{
-			tmp = head;
-			while (tmp != NULL)
+			head = prev;
+			prev =  p2;
+			while (1)
 			{
-				if (tmp->next == head)
-					return (tmp);
-				tmp = tmp->next;
+				p2 = prev;
+				while (p2->next != head && p2->next != prev)
+				{
+					p2 = p2->next;
+				}
+				if (p2->next == head)
+					break;
+
+				head = head->next;
 			}
-			head = head->next;
-		}	
+			return (p2->next);
+		}
 	}
+
 	return (NULL);
 }
