94cb11a152cb0676fe076d9edec28252592daaca
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-27 01:43:17 +0100

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -32,4 +32,5 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 int _putchar(char c);
+void free_listp(listp_t **head);
 #endif /* list.h */
