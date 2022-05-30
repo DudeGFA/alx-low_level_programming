5d441a2df0b96badf85837d9e4174b3c42045553
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-30 20:26:55 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,37 @@
 #include "main.h"
+/**
+ * binary_to_uint - converts binary to int
+ * 
+ * @b: parameter 
+ * Return: unsigned int 
+ */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int bin = 0;
-	int i, j, k, pow = 1;
-
+	int i, j, k, pow;
+	unsigned int *ptr;
 	for (i = 0; b[i] != '\0'; i++)
 	{
+	}
+	ptr = malloc(i * sizeof(int));
+	if (ptr == NULL)
+		return (0);
+	for (k = 0; k < i; k++ )
+	{
+		if (b[k] < 48 || b[k] > 49)
+			return (0);
+		ptr[k] = b[k] - '0';
 	}
 	for (j = 0; j < i; j++)
 	{
 		if (j == (i - 1))
-		{
-			bin += b[j];
-		}
+			bin += ptr[j];
 		else
 		{
-			for(k = (i - j); k > 1; k-- )	
-				pow *= 2;
-			bin += b[i] * pow;
+			pow = 1;
+			for(k = (i - j); k > 1; k--)
+				pow = pow * 2;
+			bin += ptr[j] * pow;
 		}
 	}
 	return (bin);
