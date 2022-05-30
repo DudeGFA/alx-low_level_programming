8b1a784e79acf533f5dd58f35a65aff6732f6675
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-05-30 21:33:11 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,18 +6,14 @@
  */
 void print_binary(unsigned long int n)
 {
-    if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
-    if ((n / 2) == 0)
-    {
-        _putchar('1');
-    }
-    else
-    {
-        print_binary(n / 2);
-        _putchar((n % 2) + '0');
-    }
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
 }
