043aa9145e65a4c5eeaf050bf6a09570e2224acf
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-06-01 21:53:15 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: name of file to be created
+ * @text_content: content to be copied into file\
+ * Return: 1 or -1
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int fd, i;
