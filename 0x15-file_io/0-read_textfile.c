225cd42c56d42cac57084c00572c246b6472d989
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-06-01 20:26:12 +0100

    Create 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fd, i, j;
+    char *buf;
+
+    buf = malloc(letters * sizeof(char));
+    if (buf == NULL || filename == NULL)
+    {
+        free(buf);
+        return (0);
+    }
+    fd = open(filename, O_RDONLY);
+    if (fd < 0)
+    {
+        free (buf);
+        return (0);
+    }
+    j = read (fd, buf, letters);
+    if (j < 0)
+    {
+        free(buf);
+        return (0);
+    }
+    close (fd);
+    i = write(STDOUT_FILENO, buf, j);
+    if (i < 0)
+        return (0);
+    return (i);
+}
