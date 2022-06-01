597687c04d9c9e4df4ada13320c3652fb89bc461
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-06-01 22:29:46 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,9 +20,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		{
 		}
 		k = write(fd, text_content, i);
-        if (k < 0)
-            return (-1);
-	}
+	if (k < 0)
+		return (-1);
 	close(fd);
 	return (1);
 }
