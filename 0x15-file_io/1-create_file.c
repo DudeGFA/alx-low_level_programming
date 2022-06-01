b4015dd14fd5d40dc3671e5d54f103af1bff7a0b
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-06-01 21:51:09 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,20 +1,20 @@
 #include "main.h"
 int create_file(const char *filename, char *text_content)
 {
-    int fd, i;
+	int fd, i;
 
-    if (filename == NULL)
-        return (-1);
-    fd = creat(filename, 0600);
-    if (fd < 0)
-        return (-1);
-    if (text_content != NULL)
-    {
-        for (i = 0; text_content[i]; i++)
-        {
-        }
-        write(fd, text_content, i);
-    }
-    close (fd);
-    return (1);
+	if (filename == NULL)
+		return (-1);
+	fd = creat(filename, 0600);
+	if (fd < 0)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i]; i++)
+		{
+		}
+		write(fd, text_content, i);
+	}
+	close(fd);
+	return (1);
 }
