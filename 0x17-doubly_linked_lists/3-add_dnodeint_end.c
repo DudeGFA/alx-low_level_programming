54ce6dd2f183db31d6d07ada7f1cd831e14bdaed
DudeGFA <favouridowuagida0@gmail.com>
2022-07-08 01:57:45 +0100

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,7 +7,7 @@
  * @n: value of new node
  * Return: size_t
  */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_head_node;
 
