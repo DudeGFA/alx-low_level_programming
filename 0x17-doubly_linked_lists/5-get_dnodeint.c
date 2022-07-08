718be5ee591dc0306d02431b6ac3100f969591a1
DudeGFA <favouridowuagida0@gmail.com>
2022-07-08 02:45:35 +0100

    created 5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - gets node at index
+ * @head: pointer to head of doubly liked list
+ * index: index of node to be returnd
+ * Return: node at index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    dlistint_t *h = head;
+    int i;
+
+    for (i = 1; h != NULL, i <= index; i++)
+    {
+        h = h->next;
+    }
+    if (h != NULL)
+        return (h);
+    return (NULL);
+}
