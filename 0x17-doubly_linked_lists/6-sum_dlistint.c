b1e2918d7a9484c0c68f8361d5276f8224b525fa
DudeGFA <favouridowuagida0@gmail.com>
2022-07-08 02:57:35 +0100

    created 6-sum_dlistint.c

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * sum_dlistint - sums elements of a doubly
+ * linked list
+ * @head: pointer to head of doubly liked list
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+    dlistint_t *h = head;
+    unsigned int i, sum = 0;
+
+    while (h != NULL)
+    {
+        sum += h->n;
+        h = h->next;
+    }
+    return (sum);
+}
