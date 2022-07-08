adf7403949e3dbb2fe4c0a941e98f55c9fc424b0
DudeGFA <favouridowuagida0@gmail.com>
2022-07-08 01:09:36 +0100

    update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include "lists.h"
+/**
+ * print_dlistint - prints members of a 
+ * doubly linked list
+ * @h: pointer to head of doubly liked list 
+ * Return: size_t 
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
     int i = 0;
     while (h != NULL)
     {
-        printf ("%d\n", (*h).n);
+        printf("%d\n", (*h).n);
         h = h->next;
         i++;
     }
