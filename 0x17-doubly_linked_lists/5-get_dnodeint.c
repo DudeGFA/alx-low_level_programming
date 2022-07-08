6aa2274697c95faff8780904a028ffa0e1327313
DudeGFA <favouridowuagida0@gmail.com>
2022-07-08 02:48:19 +0100

    Update 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,7 +9,7 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
     dlistint_t *h = head;
-    int i;
+    unsigned int i;
 
     for (i = 1; h != NULL, i <= index; i++)
     {
