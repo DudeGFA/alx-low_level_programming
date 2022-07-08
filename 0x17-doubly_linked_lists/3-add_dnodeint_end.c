276d0688f1e3f3a99c06c21b7f36f772ccf94e6e
DudeGFA <favouridowuagida0@gmail.com>
2022-07-08 01:56:26 +0100

    created 3-add_dnodeint_end.c

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_dnodeint_end - adds a new node at
+ * the beginning of a dlistint_t list
+ * @head: pointer to head of doubly liked list
+ * @n: value of new node
+ * Return: size_t
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+{
+	dlistint_t *new_head_node;
+
+	new_head_node = malloc(sizeof(dlistint_t));
+	if (new_head_node == NULL)
+		return (NULL);
+	new_head_node->n = n;
+	new_head_node->prev = *head;
+	new_head_node->next = NULL;
+	if ((*head) != NULL)
+		(*head)->next = new_head_node;
+	*head = new_head_node;
+	return (new_head_node);
+}
