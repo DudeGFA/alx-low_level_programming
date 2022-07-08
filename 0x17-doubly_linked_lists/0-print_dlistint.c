e2d77b1abf24ea4549221f9123caba41c0136c36
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-07-08 01:11:27 +0100

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,12 +8,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    int i = 0;
-    while (h != NULL)
-    {
-        printf("%d\n", (*h).n);
-        h = h->next;
-        i++;
-    }
-    return (i);
+	int i = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", (*h).n);
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
