f3ff038e4b61e14a916249eba08d81abf3ecd59b
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-07-12 17:31:53 +0100

    Create 3-islower.c

---
+++ 0x18-dynamic_libraries/functions/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _islower - check for lower case letter
+* @c : character to be checked
+* Return: 0 or 1
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+
+}
