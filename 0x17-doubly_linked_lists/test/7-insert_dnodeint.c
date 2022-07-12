ad4ba07b0b77a2fd734818057997327fac2def25
DudeGFA <favouridowuagida0@gmail.com>
2022-07-12 09:08:34 -0700

    created 0x18-dynamic_libraries

--- 0x17-doubly_linked_lists/test/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/test/7-insert_dnodeint.c
@@ -5,41 +5,40 @@
  * index of a doubly linked list
  * @h: pointer to head of doubly liked list
  * @n: int
+ * @idx: idx of new node
  * Return: pointer to new node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    unsigned int i;
-    dlistint_t *new, *curr = NULL;
-    dlistint_t *temp = *h;
+	unsigned int i;
+	dlistint_t *new, *curr = NULL;
+	dlistint_t *temp = *h;
 
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-        return (NULL);
-    new->n = n;
-    for (i = 0; i < idx; i++)
-    {
-        if (temp == NULL)
-        {
-            return (NULL);
-        }
-        curr = temp;
-        temp = temp->next;
-    }
-    new->next = temp;
-    new->prev = curr;
-    if (curr != NULL)
-        curr->next = new;
-    if (temp != NULL)
-        temp->prev =  new;
-    if (*h != NULL)
-    {
-        while ((*h)->prev != NULL)
-        {
-            *h = (*h)->prev;
-        }
-    }
-    else
-        *h = new;
-    return (new);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	for (i = 0; i < idx; i++)
+	{
+		if (temp == NULL)
+			return (NULL);
+		curr = temp;
+		temp = temp->next;
+	}
+	new->next = temp;
+	new->prev = curr;
+	if (curr != NULL)
+		curr->next = new;
+	if (temp != NULL)
+		temp->prev =  new;
+	if (*h != NULL)
+	{
+		while ((*h)->prev != NULL)
+		{
+			*h = (*h)->prev;
+		}
+	}
+	else
+		*h = new;
+	return (new);
 }
