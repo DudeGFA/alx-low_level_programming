50da74dc1616b8a353785b9b3675b67ba2f38f55
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-07-12 22:03:38 +0100

    Create 100-operations.c

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,50 @@
+/**
+ * add - adds two ints
+ * @a: first int
+ * @b: second int
+ * Return: a + b
+ */
+int add(a, b)
+{
+	return (a + b);
+}
+/**
+ * sub - finds diff of two ints
+ * @a: first int
+ * @b: second int
+ * Return: a - b
+ */
+int sub(a, b)
+{
+	return (a - b)
+}
+/**
+ * mul - multiplies two ints
+ * @a: first int
+ * @b: second int
+ * Return: a * b
+ */
+int mul(a, b)
+{
+	return (a * b);
+}
+/**
+ * div - divides two ints
+ * @a: first int
+ * @b: second int
+ * Return: a / b
+ */
+int div(a, b);
+{
+	return (a / b);
+}
+/**
+ * mod - return remainder of a / b
+ * @a: first int
+ * @b: second int
+ * Return: a // b
+ */
+int mod(a, b)
+{
+	return (a // b);
+}
