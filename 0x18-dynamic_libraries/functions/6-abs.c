2f65b90704ed6f216cbeabd29032a2204bb2415d
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-07-12 17:33:48 +0100

    Create 6-abs.c

---
+++ 0x18-dynamic_libraries/functions/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - prints the abs of a number
+ * @n : character to be checked
+ * Return: 0 or 1
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		n = n;
+	}
+	else if (n < 0)
+	{
+		n = n * (-1);
+	}
+	else
+	{
+		n = 0;
+	}
+	return (n);
+}
