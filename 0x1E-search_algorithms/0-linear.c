2bc00851428e97c154d5ebc8b913b52cbb6d94c3
DudeGFA <favouridowuagida0@gmail.com>
2022-11-02 20:13:08 +0100

    debugged 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,8 +15,7 @@ int linear_search(int *array, size_t size, int value)
     unsigned int i;
 
 	if (array == NULL || size == 0)
-		return (-1);
-
+	    return (-1);
 	for(i = 0; i < size; i++)
     {
         printf("Value checked array[%u] = %d\n", i, array[i]);
